d26b6b9ebb042ad2916ce4b514b88a1b5059df83
Kyle-Haugstad <khaugstad8@outlook.com>
2025-09-18 21:46:40 -0500

    added header prompt message to enter your name

--- javascript/external.js
+++ javascript/external.js
@@ -4,5 +4,8 @@
 // document.writeln(arr[i],"<br>");
 // }
 // A prompt box is used to prompt users to input a value before entering a page.
+// A prompt box is used to prompt users to input a value before entering a page.
 user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+message = `<h1>Hello, welcome ${user_name}!</h1>`;
+if(message)
+    document.getElementById("header").innerHTML = message;
