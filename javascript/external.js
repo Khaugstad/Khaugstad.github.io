eddedc789beb3cc4413fc792821b6fa69c72aa20
Kyle-Haugstad <khaugstad8@outlook.com>
2025-10-01 01:08:52 -0500

    Filled content from webmaps array into the webmaps table

--- javascript/external.js
+++ javascript/external.js
@@ -13,30 +13,43 @@ if(message)
     document.getElementById("header").innerHTML = message;
 }
 */
-function welcome()
-{
-let a = "Please enter your name.";
-let b = "Type your name here.";
-// A prompt box is used to prompt users to input a value before entering a page.
-user_name = window.prompt(a, b);
-message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
-return message
+var webmaps = [
+    ["National Geographic Mapmaker", "https://mpmkr.maps.arcgis.com/apps/instant/atlas/index.html?appid=0cd1cdee853c413a84bfe4b9a6931f0d",
+        "ArcGIS Online provides interactive mapping tools such as this instant atlas. This powerful web map opens up the possiblities\
+         to interact will all sorts of different data sets and analysis across the world."],
+    ["National Geographic Mapmaker", "https://mpmkr.maps.arcgis.com/apps/instant/atlas/index.html?appid=0cd1cdee853c413a84bfe4b9a6931f0d",
+        "ArcGIS Online provides interactive mapping tools such as this instant atlas. This powerful web map opens up the possiblities\
+        to interact will all sorts of different data sets and analysis across the world."],
+];
+function welcome() {
+    let a = "Please enter your name.";
+    let b = "Type your name here.";
+    // A prompt box is used to prompt users to input a value before entering a page.
+    user_name = window.prompt(a, b);
+    message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+    return message
 }
 document.write(welcome());
 
-function webmap_table()
-{
-document.write("<table width=100%>");
-for (var row=0; row < 2; row++)
-{
-document.write("<tr>");
-for (var column=0; column < 3; column++)
-{
-document.write("<td>" + row + "," + column + "</td>");
-}
-document.write("</tr>");
-}
-document.write("</table>");
-return "";
+function webmap_table() {
+    document.write("<table width=100%>");
+    for (var row = 0; row < webmaps.length; row++) {
+        document.write("<tr>");
+        for (var column = 0; column < webmaps[0].length; column++) {
+            if(column % 2  == 0)
+            {
+                document.write("</tr>");
+                document.write('<tr>');
+                document.write('<td colspan="2">' + webmaps[row][column] + "</td>");
+            }
+            else
+            {
+                document.write("<td>" + webmaps[row][column] + "</td>");
+            }    
+        }
+        document.write("</tr>");
+    }
+    document.write("</table>");
+    return "";
 }
 
